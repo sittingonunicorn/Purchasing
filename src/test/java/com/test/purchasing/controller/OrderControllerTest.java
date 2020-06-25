19dc892333cccfb8c0ffa6670fc009fc6c2c53e3
sittingonunicorn <sittingonunicorn@users.noreply.github.com>
2020-06-25 11:19:33 +0300

    Test

--- src/test/java/com/test/purchasing/controller/OrderControllerTest.java
+++ src/test/java/com/test/purchasing/controller/OrderControllerTest.java
@@ -1,8 +1,10 @@
 package com.test.purchasing.controller;
 
-import com.test.purchasing.model.entity.Order;
+import com.test.purchasing.model.dto.CreateOrderDTO;
 import com.test.purchasing.model.exception.GoodNotFoundException;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,10 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.support.SessionStatus;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -37,7 +37,19 @@ class OrderControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    Order order;
+    CreateOrderDTO order;
+
+    @MockBean
+    Model model;
+
+    @MockBean
+    SessionStatus sessionStatus;
+
+    @Autowired
+    OrderController orderController;
+
+    @Rule
+    public ExpectedException expectedException;
 
     @Test
     void getListPageUnauthorized() throws Exception {
@@ -92,42 +104,5 @@ class OrderControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is3xxRedirection());
-    }
-
-    @Test
-    @WithUserDetails("test@gmail.com")
-    void orderPayment() {
-
-
-    }
-
-    @Test
-    void handleNotEnoughMoneyException() {
-    }
-
-    @Test
-    void handleGoodNotFoundException() {
-    }
-
-    private String buildUrlEncodedFormEntity(String... params) {
-        if( (params.length % 2) > 0 ) {
-            throw new IllegalArgumentException("Need to give an even number of parameters");
-        }
-        StringBuilder result = new StringBuilder();
-        for (int i=0; i<params.length; i+=2) {
-            if( i > 0 ) {
-                result.append('&');
-            }
-            try {
-                result.
-                        append(URLEncoder.encode(params[i], StandardCharsets.UTF_8.name())).
-                        append('=').
-                        append(URLEncoder.encode(params[i+1], StandardCharsets.UTF_8.name()));
-            }
-            catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return result.toString();
     }
 }
